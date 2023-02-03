549efabdc97bad1dcd013ec0475206355ebcddd2
ArrowsmithLucy <lucyarrowsm1@gmail.com>
2023-02-03 16:28:41 +1100

    Added Header
    added links and decoration to header buttons.

---
+++ js/header.js
@@ -0,0 +1,13 @@
+/* 
+Js file for header functions
+*/
+
+
+/* Directs user to home page */
+function home() {
+    window.location.assign("index.html")
+}
+
+function members() {
+    window.location.assign("../html/members.html")
+}
