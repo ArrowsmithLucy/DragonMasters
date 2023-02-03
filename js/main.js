7171be3c24396f7617ca26e88e2872a26a03222d
ArrowsmithLucy <lucyarrowsm1@gmail.com>
2023-02-03 16:28:08 +1100

    Aligned home page
    added responsive design to New events.

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/*
+JS file for index.html.
+Will direct users to pages.
+Hide & show divs 
+*/
+
+
+/* Directs user to Add New Event*/
+function addNewEvent() {
+    window.location.assign("html/add-new-event.html") 
+}
+
+/* Directs user to Add New Race */
+function addNewRace() {
+    window.location.assign("html/add-new-race.html") 
+}
+
