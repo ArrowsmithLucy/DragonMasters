b9ca6c6d500e488ad2fb866b57f0174eb274bdfd
ArrowsmithLucy <lucyarrowsm1@gmail.com>
2023-02-10 16:39:38 +1100

    Resized for web size page adjust

--- js/main.js
+++ js/main.js
@@ -4,14 +4,3 @@ Will direct users to pages.
 Hide & show divs 
 */
 
-
-/* Directs user to Add New Event*/
-function addNewEvent() {
-    window.location.assign("html/add-new-event.html") 
-}
-
-/* Directs user to Add New Race */
-function addNewRace() {
-    window.location.assign("html/add-new-race.html") 
-}
-
