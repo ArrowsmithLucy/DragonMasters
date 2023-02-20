148fc26c323469b1569d7bbf895c9ba00356b437
ArrowsmithLucy <lucyarrowsm1@gmail.com>
2023-02-20 20:45:50 +1100

    Connected local storage to event display outputs

--- js/event-display.js
+++ js/event-display.js
@@ -1,4 +1,22 @@
+
 /* Event details page */
+
+let eventArray = getLocalStorage(KEY_EVENT)
+
+
+function displayDetails() {
+
+    let dateFrom = new Date(eventArray[eventIndex].dateFrom).toDateString().slice(0,10)
+    let dateTo = new Date(eventArray[eventIndex].dateTo).toDateString()
+
+    document.getElementById("eventDate").innerText = `${dateFrom} - ${dateTo}`
+    document.getElementById("eventLocation").innerText = `${eventArray[eventIndex].location}`
+
+    //Add event title
+    
+}
+
+displayDetails()
 
 //When participants button/tab is clicked, hide section
 function showParticipants() {
